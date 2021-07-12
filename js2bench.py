c4cc29db72fe7b0834e513cdeb6358c8d7927089
Ronald Cotton <roncotton@gmail.com>
2021-07-11 19:00:43 -0700

    init

---
+++ js2bench.py
@@ -0,0 +1,185 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+js2bench.py - JetStream2 Benchmark Automation in Python
+
+Copyright 2021 Ronald Cotton
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+import os
+import sys
+import json
+import time
+import numbers
+import subprocess
+import argparse
+from pathlib import Path
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+def check_requirements(_path):
+    """ system requirements to execute """
+    if os.name != 'nt':
+        print(f'Exiting.  {__file__} must run on Windows.')
+        sys.exit(-1)
+    py_ver = float('{}.{}'.format(*sys.version_info))
+    if py_ver <= 3.3:
+        print(f'Exiting. Requires newer version of python3 than {py_ver}.')
+        sys.exit(-2)
+    if not os.path.isfile(_path):
+        print(f'{_path} doesn\'t exist. Find out version of Chrome then install required file from https://chromedriver.chromium.org/downloads.')
+        sys.exit(-3)
+    chromepath = ps_command("(Get-Item (Get-ItemProperty 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\chrome.exe').'(Default)').VersionInfo.FileName")
+    if len(chromepath) == 0:
+        print(f'Chrome isn\'t installed.  Install Chrome.')
+
+
+def arguments(js2_dir):
+    """ parsing arguments for script """
+    parser = argparse.ArgumentParser(
+        description=f'JetStream2 Benchmark Automation in Python. Json results saved in {js2_dir}. See https://browserbench.org/JetStream/in-depth.html for more details.')
+    parser.add_argument(
+        '-i',
+        '--iterations',
+        type=int,
+        help='Number of Iterations (launching Chrome)',
+        default=int(1))
+    args = parser.parse_args()
+    return args.iterations
+
+
+def js2_benchmark(iteration, _path):
+    """ main JetStream2 Benchmark using selenium and chromedriver """
+    bench_site = 'https://browserbench.org/JetStream/'
+
+    # options remove 'Devtools listening ...' output
+    # https://stackoverflow.com/questions/47417581/selenium-chromedriver-how-to-disable-the-messagedevtools-on-ws/47496386
+    options = webdriver.ChromeOptions()
+    options.add_argument("--headless")
+    options.add_argument("--silent")
+    options.add_argument("--log-level=3")
+    options.add_experimental_option('excludeSwitches', ['enable-logging'])
+    driver = webdriver.Chrome(_path, options=options)
+
+    timeout = 2  # in seconds
+
+    driver.get(bench_site)
+
+    wait = WebDriverWait(driver, timeout)
+    element = wait.until(
+        EC.element_to_be_clickable(
+            (By.LINK_TEXT, 'Start Test')))
+    element.click()
+
+    benchmark_list = []
+    score_list = []
+
+    # Pulls all the ids between the id=results tags
+    results = driver.find_element_by_id('results')
+    ids = results.find_elements_by_xpath('//*[@id]')
+
+    # This will make a list of benchmark ids and main scores id,
+    # will be used to generate a dictionary for all scores.
+    getscore = False
+    for _id in ids:
+        single = _id.get_attribute('id')
+        if getscore:
+            score_list.append(single)
+        if single.startswith('benchmark'):
+            benchmark_list.append(single)
+            getscore = True
+        else:
+            getscore = False
+
+    dictionary_results = {}
+
+    for idx, score_id in enumerate(score_list):
+        result = ''
+        while not isinstance(result, numbers.Number):
+            time.sleep(timeout)
+            element = WebDriverWait(
+                driver, timeout).until(
+                EC.presence_of_element_located(
+                    (By.ID, score_id)))
+            try:
+                result = float(element.text)
+            except ValueError:
+                result = ''
+        dictionary_results[benchmark_list[idx][10:]] = result
+
+    result_summary = driver.find_element_by_id('result-summary')
+    dictionary_results['!Benchmark_Score'] = float(
+        result_summary.find_element_by_class_name("score").text)
+
+    with open(JS2_DIR + f'\\results{iteration}.json', 'w') as outfile:
+        json.dump(dictionary_results, outfile, indent=4, sort_keys=True)
+
+    print(f'Benchmark Information (saved at {JS2_DIR}\\results{iteration}.json)')
+    print(dictionary_results)
+    print(f'Benchmark Score: {dictionary_results["!Benchmark_Score"]}')
+
+    driver.quit()
+
+
+def ps_command(cmd):
+    """ executes windows powershell commands """
+    result = subprocess.run(
+        ['powershell', '-Command', cmd], capture_output=True, check=True)
+    if result.returncode == 0:
+        return str(result.stdout.decode('utf-8').rstrip('\r\n'))
+    return None
+
+
+def save_windows_info():
+    """ collect chrome and windows info """
+    system_info_dict = {}
+    system_info_dict['Chrome Version'] = ps_command(
+        '(Get-Item "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe").VersionInfo | Select-Object -ExpandProperty ProductVersion')
+    system_info_dict['Windows Edition'] = ps_command(
+        '(Get-ItemProperty "HKLM:\\SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion").ProductName')
+    system_info_dict['Windows Build'] = ps_command('(Get-ItemProperty "HKLM:\\SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion").CurrentBuild') + \
+        '.' + ps_command('(Get-ItemProperty "HKLM:\\SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion").UBR')
+    system_info_dict['Windows Version'] = ps_command(
+        '(Get-ItemProperty "HKLM:\\SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion").DisplayVersion')
+
+    with open(JS2_DIR + '\\sysinfo.json', 'w') as outfile:
+        json.dump(system_info_dict, outfile, indent=4, sort_keys=True)
+
+    print(f'System Information (saved at {JS2_DIR}\\sysinfo.json):')
+    print(system_info_dict)
+
+
+if __name__ == "__main__":
+    # make default results in C:\Users\<UserName>\Documents\JetStream2, if
+    # doesn't exist
+    _path = os.getcwd() + '\\chromedriver.exe'
+    check_requirements(_path)
+    JS2_DIR = str(Path.home()) + '\\Documents\\JetStream2'
+    iterations = arguments(JS2_DIR)
+    Path(JS2_DIR).mkdir(parents=True, exist_ok=True)
+    save_windows_info()
+    for i in range(1, iterations + 1):
+        print(f'-- iteration {i} --')
+        js2_benchmark(i, _path)
---
+++ readme.md
@@ -0,0 +1,62 @@
+<center>
+
+# js2bench
+## JetStream2 Benchmark Automation in Python
+
+[![License](https://img.shields.io/github/license/yilber/readme-boilerplate.svg)](https://github.com/Yilber/readme-boilerplate/blob/master/LICENSE)
+
+>Windows-based Selenium + Chromedriver Automation to run JetStream2 browser benchmarks using automation.  Works on completely from command line, allows multiple iterations, displays results as well as saves results in .json format.
+
+</center>
+
+## Requirements
+
+* Compatible version of Chrome installed
+* Python 3.3 - 3.9
+* Chromedriver placed in same folder as js2bench.py - download appropriate version from https://chromedriver.chromium.org/downloads
+
+## Download from Git
+
+```sh
+$ git clone https://github.com/roncotton/js2bench.git
+$ cd js2bench
+```
+
+## Execution
+
+>1. (optional - recommended) Setup some type of virtual environment for the project depending on preference.
+>2. From terminal: ```pip install -r requirements.txt```
+>3. ```python -m js2bench.py``` (Wait several minutes for benchmark to complete)
+>4. (optional parameters) ```-h``` or ```-help``` (for help) & ```-i``` or ```-iterations``` (+ number for number of iterations)  
+
+## TODO
+
+* Adding Mac and Linux Compatibility
+* Testing Framework
+
+## Scaffolding
+
+```text
+├── js2bench.py
+├── requirements.txt
+├── LICENSE
+└── README.md
+```
+
+## Bugs
+
+If you have questions, feature requests or a bug you want to report, please click [here](https://github.com/ronaldcotton/js2bench/issues) to file an issue.
+
+## Author
+
+* [**Ronald Cotton**](https://github.com/ronaldcotton)
+
+## Business Inquiry
+
+For Python Automation for your company, feel free to email me at roncotton @ gmail dot com with the subject line of "Business Inquiry."
+
+## License
+
+Copyright (c) 2021 Ron Cotton
+
+Usage is provided under the MIT License. See [LICENSE](https://github.com/ronaldcotton/js2bench//LICENSE) for the full details.
