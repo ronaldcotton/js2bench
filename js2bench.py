0dc8a8cc9cd47fca908267e5047a686794e694b0
Ron Cotton <roncotton@BornHanYolos-Compy.local>
2021-07-12 03:59:52 -0700

    v1.2.193 added MacOS Support

--- js2bench.py
+++ js2bench.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+__version__ = '1.21.193' # Major, YY, Day of Year
+__author__ = 'roncotton@gmail.com'
+
 """
 js2bench.py - JetStream2 Benchmark Automation in Python
 
@@ -30,8 +33,9 @@ import sys
 import json
 import time
 import numbers
-import subprocess
 import argparse
+import subprocess
+import platform
 from pathlib import Path
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -39,10 +43,10 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 
-def check_requirements(_path):
+def check_requirements(_os, _path):
     """ system requirements to execute """
-    if os.name != 'nt':
-        print(f'Exiting.  {__file__} must run on Windows.')
+    if _os != 'win' and _os != 'mac':
+        print(f'Exiting.  {__file__} must run on Windows or MacOS.')
         sys.exit(-1)
     py_ver = float('{}.{}'.format(*sys.version_info))
     if py_ver <= 3.3:
@@ -51,9 +55,12 @@ def check_requirements(_path):
     if not os.path.isfile(_path):
         print(f'{_path} doesn\'t exist. Find out version of Chrome then install required file from https://chromedriver.chromium.org/downloads.')
         sys.exit(-3)
-    chromepath = ps_command("(Get-Item (Get-ItemProperty 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\chrome.exe').'(Default)').VersionInfo.FileName")
-    if len(chromepath) == 0:
-        print(f'Chrome isn\'t installed.  Install Chrome.')
+    if _os == 'win':
+        chromepath = ps_command("(Get-Item (Get-ItemProperty 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\chrome.exe').'(Default)').VersionInfo.FileName")
+    else:
+        chromepath = mac_command("/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --version")
+    if chromepath is None:
+        print(f'Chrome isn\'t installed in the default location.  Install Chrome.')
 
 
 def arguments(js2_dir):
@@ -70,7 +77,7 @@ def arguments(js2_dir):
     return args.iterations
 
 
-def js2_benchmark(iteration, _path):
+def js2_benchmark(iteration, _os, _path):
     """ main JetStream2 Benchmark using selenium and chromedriver """
     bench_site = 'https://browserbench.org/JetStream/'
 
@@ -133,10 +140,15 @@ def js2_benchmark(iteration, _path):
     dictionary_results['!Benchmark_Score'] = float(
         result_summary.find_element_by_class_name("score").text)
 
-    with open(JS2_DIR + f'\\results{iteration}.json', 'w') as outfile:
+    if _os == 'win':
+        savefile = JS2_DIR + f'\\results{iteration}.json'
+    else:
+        savefile = JS2_DIR + f'/results{iteration}.json'
+
+    with open(savefile, 'w') as outfile:
         json.dump(dictionary_results, outfile, indent=4, sort_keys=True)
 
-    print(f'Benchmark Information (saved at {JS2_DIR}\\results{iteration}.json)')
+    print(f'Benchmark Information (saved at {savefile})')
     print(dictionary_results)
     print(f'Benchmark Score: {dictionary_results["!Benchmark_Score"]}')
 
@@ -144,7 +156,7 @@ def js2_benchmark(iteration, _path):
 
 
 def ps_command(cmd):
-    """ executes windows powershell commands """
+    """ executes windows powershell commands - only one liners """
     result = subprocess.run(
         ['powershell', '-Command', cmd], capture_output=True, check=True)
     if result.returncode == 0:
@@ -152,9 +164,18 @@ def ps_command(cmd):
     return None
 
 
+def mac_command(cmd):
+    """ executes mac commands - only one liners"""
+    result = subprocess.run(
+        [cmd], capture_output=True, check=True, shell=True)
+    if result.returncode == 0:
+        return str(result.stdout.decode('utf-8').rstrip('\n'))
+    return None
+
 def save_windows_info():
     """ collect chrome and windows info """
     system_info_dict = {}
+    system_info_dict['OS'] = 'Windows'
     system_info_dict['Chrome Version'] = ps_command(
         '(Get-Item "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe").VersionInfo | Select-Object -ExpandProperty ProductVersion')
     system_info_dict['Windows Edition'] = ps_command(
@@ -163,6 +184,7 @@ def save_windows_info():
         '.' + ps_command('(Get-ItemProperty "HKLM:\\SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion").UBR')
     system_info_dict['Windows Version'] = ps_command(
         '(Get-ItemProperty "HKLM:\\SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion").DisplayVersion')
+    system_info_dict['Python Version'] = ps_command('python --version')[7:]
 
     with open(JS2_DIR + '\\sysinfo.json', 'w') as outfile:
         json.dump(system_info_dict, outfile, indent=4, sort_keys=True)
@@ -171,15 +193,41 @@ def save_windows_info():
     print(system_info_dict)
 
 
+def save_mac_info():
+    """ collect chrome and mac info """
+    system_info_dict = {}
+    system_info_dict['OS'] = 'MacOS'
+    system_info_dict['Chrome Version'] = mac_command(
+        '/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --version')[14:].strip()
+    system_info_dict['MacOS Version'] = platform.mac_ver()[0]
+    system_info_dict['Python Version'] = mac_command('python3 --version')[7:]
+
+    with open(JS2_DIR + '/sysinfo.json', 'w') as outfile:
+        json.dump(system_info_dict, outfile, indent=4, sort_keys=True)
+
+    print(f'System Information (saved at {JS2_DIR}/sysinfo.json):')
+    print(system_info_dict)
+
+
 if __name__ == "__main__":
     # make default results in C:\Users\<UserName>\Documents\JetStream2, if
     # doesn't exist
-    _path = os.getcwd() + '\\chromedriver.exe'
-    check_requirements(_path)
-    JS2_DIR = str(Path.home()) + '\\Documents\\JetStream2'
+    _os = platform.platform()[:3].lower()
+    if _os == 'win':
+        _path = os.getcwd() + '\\chromedriver.exe'
+    else:
+        _path = os.getcwd() + '/chromedriver'
+    check_requirements(_os, _path)
+    if _os == 'win':
+        JS2_DIR = str(Path.home()) + '\\Documents\\JetStream2'
+    else:
+        JS2_DIR = str(Path.home()) + '/Documents/JetStream2'
     iterations = arguments(JS2_DIR)
     Path(JS2_DIR).mkdir(parents=True, exist_ok=True)
-    save_windows_info()
+    if _os == 'win':
+        save_windows_info()
+    else:
+        save_mac_info()
     for i in range(1, iterations + 1):
         print(f'-- iteration {i} --')
-        js2_benchmark(i, _path)
+        js2_benchmark(i, _os, _path)
--- readme.md
+++ readme.md
@@ -5,15 +5,15 @@
 
 [![License](https://img.shields.io/github/license/yilber/readme-boilerplate.svg)](https://github.com/Yilber/readme-boilerplate/blob/master/LICENSE)
 
->Windows-based Selenium + Chromedriver Automation to run JetStream2 browser benchmarks using automation.  Works on completely from command line, allows multiple iterations, displays results as well as saves results in .json format.
+>Windows and MacOS Selenium + Chromedriver Automation to run JetStream2 browser benchmarks using automation.  Works on completely from command line, allows multiple iterations, displays results as well as saves results in .json format.  64 different benchmark datapoints and a final synthetic benchmark is captured.  For more information about the benchmarks, read the [in-depth analysis](https://browserbench.org/JetStream2.0/in-depth.html) for details for the JetStream2 Benchmark Suite.
 
 </center>
 
 ## Requirements
 
 * Compatible version of Chrome installed
-* Python 3.3 - 3.9
-* Chromedriver placed in same folder as js2bench.py - download appropriate version from https://chromedriver.chromium.org/downloads
+* Python 3.4 - 3.9 (Tested on Python 3.8.10 and 3.9.2)
+* Chromedriver placed in same folder as js2bench.py (MacOS Users, control click chromedriver and open to allow app to run) - download appropriate version from https://chromedriver.chromium.org/downloads
 
 ## Download from Git
 
@@ -24,14 +24,27 @@ $ cd js2bench
 
 ## Execution
 
+(MacOS Users, use ```python3``` in place of ```python``` below.)
 >1. (optional - recommended) Setup some type of virtual environment for the project depending on preference.
 >2. From terminal: ```pip install -r requirements.txt```
 >3. ```python -m js2bench.py``` (Wait several minutes for benchmark to complete)
 >4. (optional parameters) ```-h``` or ```-help``` (for help) & ```-i``` or ```-iterations``` (+ number for number of iterations)  
 
+## Example Output (on MacOS)
+
+```
+python3 js2bench.py
+System Information (saved at /Users/roncotton/Documents/JetStream2/sysinfo.json):
+{'OS': 'MacOS', 'Chrome Version': '91.0.4472.114 ', 'MacOS Version': '10.16', 'Python Version': '3.9.2'}
+-- iteration 1 --
+Benchmark Information (saved at /Users/roncotton/Documents/JetStream2/results1.json)
+{'WSL': 0.448, 'UniPoker': 123.764, 'uglify-js-wtb': 12.362, 'typescript': 6.13, 'tsf-wasm': 27.778, 'tagcloud-SP': 126.252, 'string-unpack-code-SP': 227.546, 'stanford-crypto-sha256': 322.062, 'stanford-crypto-pbkdf2': 284.717, 'stanford-crypto-aes': 188.908, 'splay': 108.209, 'segmentation': 12.429, 'richards-wasm': 23.984, 'richards': 335.531, 'regexp': 164.0, 'regex-dna-SP': 286.785, 'raytrace': 236.394, 'quicksort-wasm': 181.369, 'prepack-wtb': 14.615, 'pdfjs': 55.474, 'OfflineAssembler': 46.726, 'octane-zlib': 14.887, 'octane-code-load': 455.473, 'navier-stokes': 302.767, 'n-body-SP': 419.218, 'multi-inspector-code-load': 219.119, 'ML': 33.573, 'mandreel': 47.186, 'lebab-wtb': 23.981, 'json-stringify-inspector': 81.091, 'json-parse-inspector': 71.463, 'jshint-wtb': 21.096, 'HashSet-wasm': 18.959, 'hash-map': 203.14, 'gcc-loops-wasm': 9.537, 'gbemu': 38.319, 'gaussian-blur': 143.93, 'float-mm.c': 6.795, 'FlightPlanner': 268.556, 'first-inspector-code-load': 106.564, 'espree-wtb': 19.808, 'earley-boyer': 240.174, 'delta-blue': 474.266, 'date-format-xparb-SP': 84.101, 'date-format-tofte-SP': 82.47, 'crypto-sha1-SP': 117.88, 'crypto-md5-SP': 107.203, 'crypto-aes-SP': 259.702, 'crypto': 476.358, 'coffeescript-wtb': 13.022, 'chai-wtb': 46.521, 'cdjs': 22.918, 'Box2D': 103.121, 'bomb-workers': 6.804, 'Basic': 242.486, 'base64-SP': 101.654, 'babylon-wtb': 20.536, 'Babylon': 220.495, 'async-fs': 55.819, 'Air': 164.925, 'ai-astar': 188.264, 'acorn-wtb': 18.089, '3d-raytrace-SP': 187.371, '3d-cube-SP': 172.303, '!Benchmark_Score': 71.432}
+Benchmark Score: 71.432
+```
+
 ## TODO
 
-* Adding Mac and Linux Compatibility
+* Add Linux Compatibility
 * Testing Framework
 
 ## Scaffolding
@@ -51,6 +64,11 @@ If you have questions, feature requests or a bug you want to report, please clic
 
 * [**Ronald Cotton**](https://github.com/ronaldcotton)
 
+## Version History
+
+* 1.21.193 - added MacOS Support
+* 1.0      - initial release
+
 ## Business Inquiry
 
 For Python Automation for your company, feel free to email me at roncotton @ gmail dot com with the subject line of "Business Inquiry."
@@ -60,3 +78,6 @@ For Python Automation for your company, feel free to email me at roncotton @ gma
 Copyright (c) 2021 Ron Cotton
 
 Usage is provided under the MIT License. See [LICENSE](https://github.com/ronaldcotton/js2bench//LICENSE) for the full details.
+
+## References
+* https://arstechnica.com/gadgets/2020/11/google-chrome-is-available-as-an-apple-m1-native-app-today/
